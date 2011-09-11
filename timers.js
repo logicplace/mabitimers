2f74c246a94fcbfc482a820944009ad1ae34f321
Daniel Tang <dan.y.tang@gmail.com>
2011-09-10 21:36:33 -0700

    clean up and separate DOM interaction from computation

--- timers.js
+++ timers.js
@@ -1,8 +1,11 @@
 /**
- * @preserve Javascript widget for Moongates. Last edit 2008-03-24 22:26
- * Author: Kakurady
- * 星月门脚本。最后更新 2008-03-24 22:26
- * 作者: 卡库拉迪
+ * @preserve JavaScript module for calculating Mabinogi schedules.
+ *
+ * Copyright 2011 Saiyr (me@saiyr.org)
+ *
+ * License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Original work by Kakurady (Moongates, Price) and Huijun (Rua).
  */
 
 // Schedules are current as of G10 in NA
@@ -15,10 +18,10 @@ var l18n = {
         emain: 'Emain Macha',
         taillteann: 'Taillteann',
         tara: 'Tara',
-        ceo: 'Ceo',
+        ceo: 'Ceo Island',
         ceann: 'Port Ceann'
     },
-    pryce: {
+    price: {
         tir: 'Outside Tir Chonaill Inn',
         dugald: 'Dugald Aisle Logging Camp Hut',
         dunbartonField: 'Dunbarton East Potato Field',
@@ -34,25 +37,16 @@ var l18n = {
     rua: {
         rest: 'Resting',
         work: 'Working'
-    }
+    },
+    //This value should be modified with regards to DST
+    serverOffset: -6 * 60 * 60 * 1000 - (60 * 1000 * 60) //GMT - 7
 };
 
 (function(l18n, window) {
     // Time
-    var date; //current time
-    var timePerErinnHour = 90000; //1 min 30 s
-    var timePerErinnMinute = 1500; //1.5 s
-    var timePerErinnDay = 2160000; //36 min
-
-    var serverTime; //current timeof server since unix epoch
-    var serverDate; //for display of server time only
-
-    var erinnDay;
-    var erinnHour;
-    var erinnMinute;
-
-    //This value should be modified with regards to DST
-    var serverOffset = -6 * 60 * 60 * 1000 - (60 * 1000 * 60); //GMT - 7
+    var TIME_PER_ERINN_MINUTE = 1500; // 1.5 s
+    var TIME_PER_ERINN_HOUR   = TIME_PER_ERINN_MINUTE * 60; // 1 min 30 s
+    var TIME_PER_ERINN_DAY    = TIME_PER_ERINN_HOUR * 24; // 36 min
 
     // Moongate
     var moonGateEpoch = Date.parse('Mar 23, 2008 22:21:00 GMT');
@@ -64,27 +58,16 @@ var l18n = {
             m.taillteann, m.tir, m.dunbarton, m.bangor
         ];
     }(l18n.moongate);
-    var moongateTime; // a number
-    var moongateDate; // a Date
-    var moonGate;
-    var lastmoonGate;
-    var i; // iterator for moongate labels
-    var nGatesShown = 6;
-    var nGatesChanged = false;
 
-    // Pryce
-    var pryceEpoch = Date.parse('Mar 24, 2008 00:00:00 GMT');
-    var pryceList = function(p) {
+    // Price
+    var priceEpoch = Date.parse('Mar 24, 2008 00:00:00 GMT');
+    var priceList = function(p) {
         return [
             p.tir, p.dugald, p.dunbartonField, p.dragonRuins, p.bangorBar,
             p.senMag, p.emainAlley, p.ceo, p.emainIsland, p.senMag,
             p.dragonRuins, p.barri, p.dunbartonSchool, p.dugald
         ];
-    }(l18n.pryce);
-    var pryce;
-    var lastPryce;
-    var nPryceShown = 2;
-    var nPryceChanged = false;
+    }(l18n.price);
 
     // Rua
     var ruaList = function(r) {
@@ -97,203 +80,86 @@ var l18n = {
             rest, rest, work, rest, rest, rest, work, rest, rest, work
         ];
     }(l18n.rua);
-    var rua;
-    var lastRua;
-    var nRuaShown = 2;
-    var nRuaChanged = false;
 
-    var day=' day';
-    var days=' days';
-    var today='Today';
-    var next='Next';
+    var day = ' day';
+    var days = ' days';
+    var today = 'Today';
+    var next = 'Next';
+
+    /**
+     * Determine the starting time and index into a list.
+     */
+    function listSelector(list, serverTime, epoch, nightShift) {
+        var erinnDaysPassed = (serverTime - epoch) / TIME_PER_ERINN_DAY;
+        var erinnHour = exports.serverTimeToErinnTime(serverTime).hour;
+        var startTime = serverTime - (serverTime % TIME_PER_ERINN_DAY);
+        var index = Math.floor(erinnDaysPassed) % list.length;
+
+        // If this is a "night shift" timer, then the time changes at 6pm. In
+        // addition, it before 6am, the starting point should be from the day
+        // before.
+        if (nightShift) {
+            if (erinnHour < 6) {
+                startTime -= 6 * TIME_PER_ERINN_HOUR;
+            } else {
+                startTime += 18 * TIME_PER_ERINN_HOUR;
+            }
+        }
+
+        if (index < 0) {
+            index += list.length;
+        }
+        return { time: startTime, index: index };
+    }
+
+    /**
+     * Generate a function for getting the next N items in a list given a
+     * server time as a starting point.
+     */
+    function nextItemsGenerator(list, epoch, nightShift) {
+        return function(serverTime, count) {
+            var start = listSelector(list, serverTime, epoch, nightShift);
+            var items = [];
+
+            for (var i = 0; i < count; i++) {
+                items.push({
+                    'time': new Date(start.time + TIME_PER_ERINN_DAY * i),
+                    'item': list[(i + start.index) % list.length]
+                });
+            }
+            return items;
+        };
+    }
 
     var exports = {
-        doSomething: function() {
-            date = new Date();
-
-            //add the offset (in milliseconds) to UTC to get server time
-            serverTime = date.getTime() + serverOffset;
-
-            //game time
-            erinnHour = Math.floor(serverTime / timePerErinnHour) % 24;
-            erinnMinute = Math.floor(serverTime / timePerErinnMinute) % 60;
-
-            //erinnDay = Math.floor(serverTime / timePerErinnDay) % 40;
-
-            //moongate target
-            moonGate = Math.floor(
-                (serverTime - moonGateEpoch) / timePerErinnDay % moonGateList.length
-            );    if (moonGate < 0) {moonGate += moonGateList.length;};
-            pryce = Math.floor(
-                (serverTime - pryceEpoch) / timePerErinnDay % pryceList.length
-            );    if (pryce < 0) {pryce += pryceList.length;};
-            rua = Math.floor(
-                (serverTime - moonGateEpoch) / timePerErinnDay % ruaList.length
-            );    if (rua < 0) {rua += ruaList.length;};
-
-            //convert the milliseconds back to Date for easy displaying
-            serverDate = new Date(serverTime);
-
-
-            //access the document
-            //textContent is not supported by IE 7
-            document.getElementById('tGameTime').firstChild.nodeValue = erinnHour + ((erinnMinute < 10)?':0':':') + erinnMinute ;
-            document.getElementById('trealtime1').firstChild.nodeValue = date.getHours() + ((date.getMinutes() < 10)?':0':':') + date.getMinutes();
-            document.getElementById('trealtime2').firstChild.nodeValue = serverDate.getUTCHours() + ((serverDate.getUTCMinutes() < 10)?':0':':') + serverDate.getUTCMinutes();
-            // document.getElementById('date').firstChild.nodeValue = 'Day '+ erinnDay;
-
-            //TODO: adjust how many moongate we have easier
-            //TODO: No need to parse the gates each time this runs.
-            if(nGatesChanged || moonGate != lastmoonGate) {
-
-                moongateTime = serverTime - Math.floor(serverTime % timePerErinnDay); //the server time for the current Erinn day's midnight
-                moongateTime += (
-                    (erinnHour < 6)? //yesterday's gate?
-                        -6*timePerErinnHour :
-                        18*timePerErinnHour)
-                    - serverOffset; //  so it's UTC again
-
-                for(i=0; i < nGatesShown ; moongateTime += timePerErinnDay, i++) {
-                    moongateDate  = new Date(moongateTime); // so as not to worry about converting dates!
-
-                    //Moongate Time
-                    document.getElementById('mgtm'+i).firstChild.nodeValue = moongateDate.getHours() + ((moongateDate.getMinutes() < 10)?':0':':') + moongateDate.getMinutes();
-                    //Moongate Destination
-                    document.getElementById('mgtxt'+i).firstChild.nodeValue = moonGateList[(moonGate + i) % moonGateList.length];
+        getServerTimeMillis: function(date) {
+            return (date || new Date()).getTime() + l18n.serverOffset;
+        },
+        getMoonGates: nextItemsGenerator(moonGateList, moonGateEpoch, true),
+        getPriceLocations: nextItemsGenerator(priceList, priceEpoch, false),
+        getRuaStatuses: nextItemsGenerator(ruaList, moonGateEpoch, true),
+        serverTimeToErinnTime: function(serverTime) {
+            return {
+                'hour': Math.floor(serverTime / TIME_PER_ERINN_HOUR) % 24,
+                'minute': Math.floor(serverTime / TIME_PER_ERINN_MINUTE) % 60
+            };
+        },
+        getNextCeoMoonGate: function(serverTime) {
+            var moonGatesFromNow = this.getMoonGates(serverTime,
+                                                     moonGateList.length);
+            for (var i = 0; i < moonGatesFromNow.length; i++) {
+                if (moonGatesFromNow[i].item === l18n.moongate.ceo) {
+                    return i;
                 }
-                for(i=0; i<moonGateList.length; i++) {
-                    if (moonGateList[(moonGate + i) % moonGateList.length] == l18n.moongate.ceo) { // ceo
-                        document.getElementById('mgtxtceo').firstChild.nodeValue = i + ((i==1)?day: days);
-                        break;
-                    }
-                }
-
-                lastmoonGate = moonGate;
-                nGatesChanged = false;
-            }
-            if(nPryceChanged|| pryce != lastPryce) {
-
-                moongateTime = serverTime - Math.floor(serverTime % timePerErinnDay); //the server time for the current Erinn day's midnight
-                moongateTime -= serverOffset;
-
-                for(i=0; i < nPryceShown ; moongateTime += timePerErinnDay, i++) {
-                    moongateDate  = new Date(moongateTime); // so as not to worry about converting dates!
-
-                    //Moongate Time
-                    document.getElementById('prtm'+i).firstChild.nodeValue = moongateDate.getHours() + ((moongateDate.getMinutes() < 10)?':0':':') + moongateDate.getMinutes();
-                    //Moongate Destination
-                    document.getElementById('prtxt'+i).firstChild.nodeValue = pryceList[(pryce + i) % pryceList.length];
-                }
-                lastPryce = pryce;
-                nPryceChanged = false;
-            }
-            //setContent(s, d.getTime());
-
-            if(nRuaChanged|| rua != lastRua) {
-
-                moongateTime = serverTime - Math.floor(serverTime % timePerErinnDay); //the server time for the current Erinn day's midnight
-                moongateTime += (
-                    (erinnHour < 6)? //yesterday's gate?
-                        -6*timePerErinnHour :
-                        18*timePerErinnHour)
-                    - serverOffset; //  so it's UTC again
-
-                for(i=0; i < nRuaShown; moongateTime += timePerErinnDay, i++) {
-
-                    moongateDate  = new Date(moongateTime); // so as not to worry about converting dates!
-
-                    //Rua Time
-                    document.getElementById('ruatm'+i).firstChild.nodeValue = moongateDate.getHours() + ((moongateDate.getMinutes() < 10)?':0':':') + moongateDate.getMinutes();
-                    //Rua Status
-                    document.getElementById('ruatxt'+i).firstChild.nodeValue = ruaList[(rua + i) % ruaList.length];
-                }
-                lastRua = rua;
-                nRuaChanged = false;
             }
         }
     };
     // Closure compiler exports. Used to preserve names on minification.
     window['mabiTimers'] = exports;
-    exports['doSomething'] = exports.doSomething;
+    exports['getServerTimeMillis'] = exports.getServerTimeMillis;
+    exports['getMoonGates'] = exports.getMoonGates;
+    exports['getPriceLocations'] = exports.getPriceLocations;
+    exports['getRuaStatuses'] = exports.getRuaStatuses;
+    exports['serverTimeToErinnTime'] = exports.serverTimeToErinnTime;
+    exports['getNextCeoMoonGate'] = exports.getNextCeoMoonGate;
 })(l18n, window);
-
-window.setInterval(mabiTimers.doSomething, 200);
-
-// DOM helpers
-window['addRow'] = function(myTop, myPrefix) {
-    var myRow; var myCell1; var myCell2; var myCell3;
-    var myShown;
-    switch (myPrefix) {
-    case 'mg':myShown = nGatesShown; break;
-    case 'pr':myShown = nPryceShown; break;
-    case 'rua':myShown = nRuaShown; break;
-    default:
-    }
-    myRow = document.createElement('TR');
-    myRow.id=myPrefix+'Row'+myShown;
-    myRow.className='mgrow';
-    //myCell1 = document.createElement('TD');
-    //myCell1.id=myPrefix+'lbl'+myShown;
-    //myCell1.className='mglabel';
-    myCell2 = document.createElement('TD');
-    myCell2.id=myPrefix+'tm'+myShown;
-    myCell2.className='mglabel2';
-    myCell3 = document.createElement('TD');
-    myCell3.id=myPrefix+'txt'+myShown;
-    myCell3.className='mgtext';
-
-    //myCell1.appendChild(document.createTextNode('in ' + myShown + ' days'));
-    myCell2.appendChild(document.createTextNode('one'));
-    myCell3.appendChild(document.createTextNode('moment...'));
-    //myRow.appendChild(myCell1);
-    myRow.appendChild(myCell2);
-    myRow.appendChild(myCell3);
-
-    document.getElementById(myTop).appendChild(myRow);
-    switch (myPrefix) {
-    case 'mg':
-        nGatesShown++;
-        nGatesChanged = true; break;
-    case 'pr':
-        nPryceShown++;
-        nPryceChanged = true; break;
-    case 'rua':
-        nRuaShown++;
-        nRuaChanged = true; break;
-    default:
-    }
-    mabiTimers.doSomething();
-},
-window['removeRow'] = function() {
-    //TODO:Check if rows can be removed
-    var myRow;
-
-    if (nGatesShown > 2) {
-        nGatesShown--;
-        myRow = document.getElementById('mgRow'+nGatesShown);
-        document.getElementById('mgtblbody0').removeChild(myRow);
-        nGatesChanged = true;
-    }
-},
-window['removeRow1'] = function() {
-    //TODO:Check if rows can be removed
-    var myRow;
-
-    if (nPryceShown > 2) {
-        nPryceShown--;
-        myRow = document.getElementById('prRow'+nPryceShown);
-        document.getElementById('prtblbody0').removeChild(myRow);
-        nPryceChanged = true;
-    }
-},
-window['removeRow2'] = function() {
-    //TODO:Check if rows can be removed
-    var myRow;
-
-    if (nRuaShown > 2) {
-        nRuaShown--;
-        myRow = document.getElementById('ruaRow'+nRuaShown);
-        document.getElementById('ruatblbody0').removeChild(myRow);
-        nRuaChanged = true;
-    }
-}
---
+++ timers_ui.js
@@ -0,0 +1,133 @@
+// DOM helpers
+function addRow(myTop, myPrefix) {
+    var myRow; var myCell1; var myCell2; var myCell3;
+    var myShown;
+    switch (myPrefix) {
+    case 'mg':myShown = nGatesShown; break;
+    case 'pr':myShown = nPryceShown; break;
+    case 'rua':myShown = nRuaShown; break;
+    default:
+    }
+    myRow = document.createElement('TR');
+    myRow.id=myPrefix+'Row'+myShown;
+    myRow.className='mgrow';
+    //myCell1 = document.createElement('TD');
+    //myCell1.id=myPrefix+'lbl'+myShown;
+    //myCell1.className='mglabel';
+    myCell2 = document.createElement('TD');
+    myCell2.id=myPrefix+'tm'+myShown;
+    myCell2.className='mglabel2';
+    myCell3 = document.createElement('TD');
+    myCell3.id=myPrefix+'txt'+myShown;
+    myCell3.className='mgtext';
+
+    //myCell1.appendChild(document.createTextNode('in ' + myShown + ' days'));
+    myCell2.appendChild(document.createTextNode('one'));
+    myCell3.appendChild(document.createTextNode('moment...'));
+    //myRow.appendChild(myCell1);
+    myRow.appendChild(myCell2);
+    myRow.appendChild(myCell3);
+
+    document.getElementById(myTop).appendChild(myRow);
+    switch (myPrefix) {
+    case 'mg':
+        nGatesShown++;
+        nGatesChanged = true; break;
+    case 'pr':
+        nPryceShown++;
+        nPryceChanged = true; break;
+    case 'rua':
+        nRuaShown++;
+        nRuaChanged = true; break;
+    default:
+    }
+    doSomething();
+}
+function removeRow() {
+    var myRow;
+
+    if (nGatesShown > 2) {
+        nGatesShown--;
+        myRow = document.getElementById('mgRow'+nGatesShown);
+        document.getElementById('mgtblbody0').removeChild(myRow);
+        nGatesChanged = true;
+    }
+}
+function removeRow1() {
+    var myRow;
+
+    if (nPryceShown > 2) {
+        nPryceShown--;
+        myRow = document.getElementById('prRow'+nPryceShown);
+        document.getElementById('prtblbody0').removeChild(myRow);
+        nPryceChanged = true;
+    }
+}
+function removeRow2() {
+    var myRow;
+
+    if (nRuaShown > 2) {
+        nRuaShown--;
+        myRow = document.getElementById('ruaRow'+nRuaShown);
+        document.getElementById('ruatblbody0').removeChild(myRow);
+        nRuaChanged = true;
+    }
+}
+
+function formatTime(hours, minutes) {
+    var hoursPrefix = hours < 10 ? '0' : '';
+    var minutesPrefix = minutes < 10 ? ':0' : ':';
+    return hoursPrefix + hours + minutesPrefix + minutes;
+}
+
+var nGatesShown = 6;
+var nPryceShown = 2;
+var nRuaShown = 2;
+
+function updateCells(items, timePrefix, textPrefix) {
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        var time = item['time'];
+        document.getElementById(timePrefix + i).firstChild.nodeValue =
+            formatTime(time.getUTCHours(), time.getUTCMinutes());
+        document.getElementById(textPrefix + i).firstChild.nodeValue =
+            item.item;
+    }
+}
+
+function doSomething() {
+    var date = new Date();
+
+    //add the offset (in milliseconds) to UTC to get server time
+    var serverTime = mabiTimers.getServerTimeMillis(date);
+
+    //game time
+    var erinnTime = mabiTimers.serverTimeToErinnTime(serverTime);
+    var erinnHour = erinnTime.hour;
+    var erinnMinute = erinnTime.minute;
+    //erinnDay = Math.floor(serverTime / TIME_PER_ERINN_DAY) % 40;
+
+    //convert the milliseconds back to Date for easy displaying
+    var serverDate = new Date(serverTime);
+
+    //access the document
+    //textContent is not supported by IE 7
+    document.getElementById('tGameTime').firstChild.nodeValue =
+        formatTime(erinnHour, erinnMinute);
+    document.getElementById('trealtime1').firstChild.nodeValue =
+        formatTime(date.getHours(), date.getMinutes());
+    document.getElementById('trealtime2').firstChild.nodeValue =
+        formatTime(serverDate.getUTCHours(), serverDate.getUTCMinutes());
+
+    // document.getElementById('date').firstChild.nodeValue = 'Day '+ erinnDay;
+
+    var moonGates = mabiTimers.getMoonGates(serverTime, nGatesShown);
+    var pryceLocations = mabiTimers.getPryceLocations(serverTime, nPryceShown);
+    var ruaStatuses = mabiTimers.getRuaStatuses(serverTime, nRuaShown);
+
+    updateCells(moonGates, 'mgtm', 'mgtxt');
+    updateCells(pryceLocations, 'prtm', 'prtxt');
+    updateCells(ruaStatuses, 'ruatm', 'ruatxt');
+}
+
+window.setInterval(doSomething, 200);
